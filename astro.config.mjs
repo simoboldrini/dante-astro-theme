7283ec73b27cb71bee81282dd05186b56870a132
Simone Boldrini <99870617+simoboldrini@users.noreply.github.com>
2026-05-18 14:28:26 +0000

    Upgrade Astro 6 compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
 import icon from "astro-icon";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
     integrations: [
-        [icon()],
+        icon(),
         mdx(),
-        sitemap(),
-        tailwind({
-            applyBaseStyles: false
-        })
-    ]
+        sitemap()
+    ],
+    vite: {
+        plugins: [tailwindcss()]
+    }
 });
